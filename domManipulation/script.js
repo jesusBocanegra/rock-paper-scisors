95fe8ab1d619331c2a404ff1b62e26279b69c8da
Jesus Bocanegra <jdej.bocanegra.l@gmail.com>
2021-02-19 15:59:59 -0600

    updated version

--- domManipulation/script.js
+++ domManipulation/script.js
@@ -1,134 +1,103 @@
+let resultsContainer = document.querySelector('#results');
+		
+let divRock = document.createElement('div');
+let buttonRock = document.createElement('button');
+let divPaper = document.createElement('div');
+let buttonPaper = document.createElement('button');
+let divScissors = document.createElement('div');
+let buttonScissors = document.createElement('button');
+buttonRock.id = "rock";
+buttonRock.textContent = 'I choose Rock';
+buttonPaper.id = "paper";
+buttonPaper.textContent = 'I choose Paper';
+buttonScissors.id = "scissors";
+buttonScissors.textContent = 'I choose Scissors';
+
+let divResults = document.createElement('div');
+let printResults = document.createElement('p');
+let divPlayerScore = document.createElement('div');
+let printPlayerScore = document.createElement('p');
+let divComputerScore = document.createElement('div');
+let printComputerScore = document.createElement('p');
+
+let computerGame = '';
+let playerSelection = '';
+let gamesL = 0;
+let gamesW = 0;
+
 function computerPlay() {
-			let comp = Math.floor(Math.random() * (+3 + 1 - +1)) + +1;
-			if (comp == 1) {
-				return "rock";
-			}
-			if (comp == 2) {
-				return "paper";
-			}
-			else {
-				return "scissors";
-			}
+		let comp = Math.floor(Math.random() * (+3 + 1 - +1)) + +1;
+		if (comp == 1) {
+			return computerGame = "rock";
 		}
-		
-		let gamesL = 0;
-		let gamesW = 0;
-		
-		function finalmessage() {
-			if ((gamesL ==5 || gamesW ==5) && (gamesL>gamesW)) {
-				alert ('you lost')
-			}
-			if ((gamesL ==5 || gamesW ==5) && (gamesL<gamesW)) {
-				alert ('You won!')
-			}
+		if (comp == 2) {
+			return computerGame = "paper";
 		}
-		
-		function youLost() {
-            printResults.textContent = 'You lost';
-            gamesL = gamesL + 1;
-			printComputerScore.textContent = "Games won by the computer: " +
-gamesL;
+		if (comp == 3) {
+			return computerGame = "scissors";
 		}
-		
-		function youWon() {
-			printResults.textContent = 'You won!';
-			gamesW = gamesW + 1;
-			printPlayerScore.textContent = 'Games won by you: ' + gamesW;		
+}
+
+function finalmessage() {
+	if ((gamesL ==5 || gamesW ==5) && (gamesL>gamesW)) {
+		alert ('You lost, press F5 to play again')
+	}
+	if ((gamesL ==5 || gamesW ==5) && (gamesL<gamesW)) {
+		alert ('You won!, press F5 to play again')
+	}
+}
+
+function playRound(){
+	while (gamesL<5 && gamesW<5) {
+		computerPlay();
+		if ((computerGame == "paper" && playerSelection == "scissors") || 
+(computerGame == "rock" && playerSelection == "paper") || (computerGame == 
+"scissors" && playerSelection == "rock")) {
+		printResults.textContent = 'You won!, computer choose: ' + computerGame;
+		gamesW = gamesW + 1;
+		printPlayerScore.textContent = 'Games won by you: ' + gamesW;
 		}
-		
-		function rockSelection(){
-		while (gamesL <5 && gamesW <5){
-			if (computerPlay() == "paper") {
-                youLost();
-                computerPlay();
-			}
-            else if (computerPlay() == "scissors") {
-				youWon();
-                computerPlay();
-			}
-            else {
-				outcome = "It's a tie!";
-				printResults.textContent = outcome;
-				computerPlay();
-            }
-            break
-            }
-           finalmessage();
+		else if ((computerGame == "scissors" && playerSelection == "paper") 
+|| (computerGame == "rock" && playerSelection == "scissors") || 
+(computerGame == "paper" && playerSelection == "rock")) {
+		printResults.textContent = 'You lost, computer choose: '+ computerGame;
+		gamesL = gamesL + 1;
+		printComputerScore.textContent = "Games won by the computer: " + gamesL;
 		}
-		
-		function paperSelection() {
-		while (gamesL <5 && gamesW <5) {
-			if (computerPlay() == "scissors") {
-                youLost();
-                computerPlay();
-            }
-            else if (computerPlay() == "rock") {
-                youWon();
-                computerPlay();
-            }
-            else {
-                outcome = "It's a tie!";
-                printResults.textContent = outcome;
-                computerPlay();
-            }
-            break
-			}
-			finalmessage();
+		else {
+		printResults.textContent = 'Tied game, computer also choose: '+ 
+computerGame;
 		}
-		
-		function scissorsSelection(){
-		while (gamesL <5 && gamesW <5) {
-			if (computerPlay() == "rock") {
-                youLost();
-                computerPlay();
-            }
-            else if (computerPlay() == "paper") {
-				youWon();                
-                computerPlay();
-            }
-            else {
-                outcome = "It's a tie!";
-                printResults.textContent = outcome;
-                computerPlay();
-            }
-            break
-			}
-			finalmessage();
-		}		
-		
-		let resultsContainer = document.querySelector('#results');
-		
-		let divRock = document.createElement('div');
-		let buttonRock = document.createElement('button');
-		let divPaper = document.createElement('div');
-		let buttonPaper = document.createElement('button');
-		let divScissors = document.createElement('div');
-		let buttonScissors = document.createElement('button');
-		buttonRock.textContent = 'I choose Rock';
-		buttonPaper.textContent = 'I choose Paper';
-		buttonScissors.textContent = 'I choose Scissors';
-		
-		let divResults = document.createElement('div');
-		let printResults = document.createElement('p');
-		let divPlayerScore = document.createElement('div');
-		let printPlayerScore = document.createElement('p');
-		let divComputerScore = document.createElement('div');
-		let printComputerScore = document.createElement('p');
-		
-		resultsContainer.appendChild(divRock);
-		resultsContainer.appendChild(divPaper);
-		resultsContainer.appendChild(divScissors);
-		divRock.appendChild(buttonRock);
-		divPaper.appendChild(buttonPaper);
-		divScissors.appendChild(buttonScissors);
-		resultsContainer.appendChild(divResults);
-		resultsContainer.appendChild(divPlayerScore);
-		resultsContainer.appendChild(divComputerScore);
-		
-		buttonRock.addEventListener('click', rockSelection);
-		buttonPaper.addEventListener('click', paperSelection);
-		buttonScissors.addEventListener('click', scissorsSelection);
-				
-		divResults.appendChild(printResults)
-		divPlayerScore.appendChild(printPlayerScore)
-		divComputerScore.appendChild(printComputerScore)
+	break;
+	}
+	finalmessage();
+}
+
+buttonRock.addEventListener('click', () => {
+	playerSelection = buttonRock.id;
+	playRound();
+});
+
+buttonPaper.addEventListener('click', () => {
+	playerSelection = buttonPaper.id;
+	playRound();
+});
+
+buttonScissors.addEventListener('click', () => {
+	playerSelection = buttonScissors.id;
+	playRound();
+});
+
+resultsContainer.appendChild(divRock);
+resultsContainer.appendChild(divPaper);
+resultsContainer.appendChild(divScissors);
+divRock.appendChild(buttonRock);
+divPaper.appendChild(buttonPaper);
+divScissors.appendChild(buttonScissors);
+resultsContainer.appendChild(divResults);
+resultsContainer.appendChild(divPlayerScore);
+resultsContainer.appendChild(divComputerScore);
+
+divResults.appendChild(printResults)
+divPlayerScore.appendChild(printPlayerScore)
+divComputerScore.appendChild(printComputerScore)
